918d97a67c5ea8f4766800b938a6bd44016dcfd8
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2023-03-30 23:29:18 -0500

    vs code debug config

--- frontend_canvas/src/settings.js
+++ frontend_canvas/src/settings.js
@@ -1165,7 +1165,7 @@ const Settings = (props) => {
     <div className="settingsOverlay">
       <div className="settingsContainer">
       <h2 className="settingsHeading">
-        Settings :
+        Settings x:
       </h2>
 
       <Form onSubmit={props.onConfirm} className="form">
