69ff5d47a31a82359131c52e91a545d5eeea570a
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2023-11-16 00:33:25 -0500

    ...

--- frontend_canvas_vite/vite.config.js
+++ frontend_canvas_vite/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import reactRefresh from '@vitejs/plugin-react-refresh';
 import svgr from "vite-plugin-svgr";
 
 const sourceJSPattern = /\/src\/.*\.js$/;
@@ -58,53 +57,3 @@ export default defineConfig({
     host: true,
   },
 });
-
-// export default {
-//   base: '/vite-react-app/',
-//   plugins: [reactRefresh(), react()],
-// };
-
-// vite.config.js
-// import fs from "node:fs";
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import * as esbuild from "esbuild";
-
-// const sourceJSPattern = /\/src\/.*\.js$/;
-// const rollupPlugin = (matchers) => ({
-//   name: "js-in-jsx",
-//   load(id) {
-//     if (matchers.some(matcher => matcher.test(id))) {
-//       const file = fs.readFileSync(id, { encoding: "utf-8" });
-//       return esbuild.transformSync(file, { loader: "jsx" });
-//     }
-//   }
-// });
-
-// export default defineConfig({
-//   plugins: [
-//     react()
-//   ],
-//   build: {
-//     rollupOptions: {
-//       plugins: [
-//         rollupPlugin([sourceJSPattern])
-//       ],
-//     },
-//     commonjsOptions: {
-//       transformMixedEsModules: true,
-//     },
-//   },
-//   optimizeDeps: {
-//     esbuildOptions: {
-//       loader: {
-//         ".js": "jsx",
-//       },
-//     },
-//   },
-//   esbuild: {
-//     loader: "jsx",
-//     include: [sourceJSPattern],
-//     exclude: [],
-//   },
-// });
