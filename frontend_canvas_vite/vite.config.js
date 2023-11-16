e92507e0dff746e570e58c729e9e05327d93a9de
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2023-11-16 00:47:25 -0500

    ...

--- frontend_canvas_vite/vite.config.js
+++ frontend_canvas_vite/vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-const sourceJSPattern = /\/src\/.*\.js$/;
-// const rollupPlugin = (matchers) => ({
-//   name: "js-in-jsx",
-//   load(id) {
-//     if (matchers.some(matcher => matcher.test(id))) {
-//       const file = fs.readFileSync(id, { encoding: "utf-8" });
-//       return esbuild.transformSync(file, { loader: "jsx" });
-//     }
-//   }
-// });
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
