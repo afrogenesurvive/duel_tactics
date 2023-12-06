fddc24ef86292e1911bfd287460c47b5ec63d8ba
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2023-12-06 18:23:22 -0500

    ...

--- frontend_canvas_vite/vite.config.js
+++ frontend_canvas_vite/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // const sourceJSPattern = /\/src\/.*\.js$/;
@@ -15,7 +15,7 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
-    // react(),
+    react(),
     svgr({
       svgrOptions: {
         ref: true,
