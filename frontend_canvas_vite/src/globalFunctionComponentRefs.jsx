ad914ba29d01ebf4505032aebe62b3f0e4c1ede9
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2025-06-30 18:38:36 -0500

    added refs component for callable global function components,
    updated draw grid init call from componentDidMount,
    updated DrawGridInit internal draw grid function basic setup

---
+++ frontend_canvas_vite/src/globalFunctionComponentRefs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const globalFunctionComponentRefs = {
+  drawGridInitRef: React.createRef()
+  // Add more refs as needed
+};
+
+export default globalFunctionComponentRefs;
