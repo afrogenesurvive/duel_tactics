b9b7c9e71cffb1f62b3bf72c9db436b2d5557f35
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2026-02-05 17:43:48 -0500

    ...

--- frontend_canvas_vite/src/main.jsx
+++ frontend_canvas_vite/src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { GameContextProvider } from "./gameContext";
-
 import "bootstrap/dist/css/bootstrap.css";
-
 // import './index.css'
 
 import refreshRuntime from "react-refresh/runtime";
@@ -16,14 +13,6 @@ window.$RefreshSig$ = () => (type) => type;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GameContextProvider>
-      <App />
-    </GameContextProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
-
-
-
-
-
-
