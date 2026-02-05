2d46b0cfcf4bf60b6afe67b4aeb83c7c9c2f96b4
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2026-02-05 18:04:12 -0500

    Revert "..."
    This reverts commit 8a904e7503d8516ef32bacdb4c6da058683b3672.

--- frontend_canvas_vite/src/globalFunctionComponents/checkCell.jsx
+++ frontend_canvas_vite/src/globalFunctionComponents/checkCell.jsx
@@ -1,23 +1,33 @@
-import React, { useContext, useRef, useEffect } from "react";
+import React, { 
+  useContext, 
+  useRef, 
+  useEffect,
+ } from "react";
 import { GameContext } from "../gameContext";
 import { imageRefs } from "../imageRefs";
 import path from "path";
+
 
 const UpdatePathArray = () => {
   const { context, setState } = useContext(GameContext);
 
   console.log("UpdatePathArray: game context", context.state);
+  
 
-  useEffect(() => {
-    if (!context.state.canvas || !context.state.canvasContext) return;
+    useEffect(() => {
 
-    console.log("UpdatePathArray useEffect triggered");
+      if (!context.state.canvas || !context.state.canvasContext) return;
 
+      console.log("UpdatePathArray useEffect triggered");
+
+      
     //   setState(prev => ({
     //     ...prev,
     //     // ...
     //   }));
-  }, [context.global_function_component_triggers.updatePathArray]); // <--- dependency
+
+    }, [context.global_function_component_triggers.updatePathArray]); // <--- dependency
+
 
   return null;
 };
